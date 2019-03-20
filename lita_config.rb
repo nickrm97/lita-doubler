26e0eb89931e0ea440672d379f7b78902acb534a
nickrm97 <nicholasrmills@icloud.com>
2019-03-20 13:51:11 +1100

    readding url

--- lita_config.rb
+++ lita_config.rb
@@ -37,7 +37,7 @@ Lita.configure do |config|
   if ENV.fetch('RACK_ENV', 'development') == 'production'
     config.robot.adapter = :slack
     config.adapters.slack.token = ENV.fetch('SLACK_API_KEY', '')
-    # config.redis[:url] = ENV.fetch('REDIS_URL')
+    config.redis[:url] = ENV.fetch('REDIS_URL')
   else
     config.robot.adapter = :shell
   end
